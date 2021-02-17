ded6206b538bb6faf9e69f7954b5fd650e545e1b
Daven <daven@comet.ml>
2021-02-17 10:18:47 -0500

    testing flow without client secret

--- src/main/java/com/oauth/OauthTest.java
+++ src/main/java/com/oauth/OauthTest.java
@@ -15,31 +15,26 @@ import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Base64;
 
 public class OauthTest {
 
     //NEED TO UPDATE THIS BASED ON AUTH SYSTEM
-    private static final String clientId = "1018158525168-bgevh35rqj4fvut43il2pq7cckosvc77.apps.something.com";
-    private static final String clientSecret = "NUjYgkt15D_l7Uh6rQi6mkD6asdasdasd";
-    private static final String accessTokenUrl = "https://dev-example.okta.com/oauth2/default/v1/token/";
-    private static final String authorizationBaseUrl = "https://dev-example.okta.com/oauth2/default/v1/authorize";
-    private static final String protectedResourceUrl = "https://yourNetwork/oauth2/v3/userinfo"; // similar to https://connect2id.com/products/server/docs/api/userinfo
-
-    //JWT SUPPORT NEED TO UPDATE THIS:
-    private static final String hmacShaKeyForJWT = "someSecret";
+    private static final String clientId = "*** Please enter Client ID Here ***";
+    private static final String accessTokenUrl = "*** Please enter Token URL Here ***";
+    private static final String authorizationBaseUrl = "*** Please enter Authorize URL Here ***";
+    private static final String callBackUrl = "http://localhost";
 
     //NO ACTION NEEDE HERE
     private static final String NETWORK_NAME = "IDA";
     private static final String secretState = "secret" + new Random().nextInt(999_999);
-    public final static ObjectMapper objectMapper = new ObjectMapper();
 
     public static void main(String args[]) throws Exception {
 
         final OAuth20Service service = new ServiceBuilder(clientId)
-                .apiSecret(clientSecret)
                 .scope("profile email") // replace with desired scope
                 .state(secretState)
-                .callback("https://www.comet.ml/google/google_oauth_callback")
+                .callback(callBackUrl)
                 .build(new OAuthProvider(accessTokenUrl, authorizationBaseUrl));
 
         final Scanner in = new Scanner(System.in, "UTF-8");
@@ -65,35 +60,13 @@ public class OauthTest {
         System.out.println("Got the Access Token! type: " + accessToken.getTokenType());
         System.out.println("(The raw response looks like this: " + accessToken.getRawResponse() + "')");
 
+
         System.out.println("Lets check if we can read the  JWT Access Token...");
-        Jws<Claims> claims = jwtSupport(accessToken.getTokenType());
-        String claimsJsonString = objectMapper.writeValueAsString(claims.getBody());
+        String claimsJsonString = jwtDecodeSupport(accessToken.getTokenType());
         System.out.println(claimsJsonString);
-
-        //OPTIONAL STEP
-        System.out.println("Refreshing the Access Token...");
-        accessToken = service.refreshAccessToken(accessToken.getRefreshToken());
-        System.out.println("Refreshed the Access Token!");
-        System.out.println("(The raw response looks like this: " + accessToken.getRawResponse() + "')");
-        System.out.println();
-
-
-        // Now let's go and ask for a protected resource!
-        System.out.println("Now we're going to access a protected resource...");
-        final OAuthRequest request = new OAuthRequest(Verb.GET, protectedResourceUrl);
-        service.signRequest(accessToken, request);
-        final Response response = service.execute(request);
-        String userProfileResponse = response.getBody();
-
-        System.out.println("User profile information: ");
-        System.out.println(userProfileResponse);
     }
 
-
-    private static Jws<Claims> jwtSupport(String jwtToken) {
-        SecretKey key = Keys.hmacShaKeyFor(hmacShaKeyForJWT.getBytes(StandardCharsets.UTF_8));
-        return Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken);
+    private static String jwtDecodeSupport(String jwtToken) {
+        return Base64.getDecoder().decode(jwtToken).toString();
     }
-
-
 }
